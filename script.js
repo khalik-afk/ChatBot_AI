9081fb6877ae4c7892bb1317d32c9039bad00a40
khalik-afk <khaliksyahiir123@gmail.com>
2025-05-15 10:13:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ function getResponse() {
 
   if (input.toLowerCase().indexOf("hai") !== -1) {
     output.innerHTML = "iya ada apa nih?";
-  } else if (input.toLowerCase().indexOf("mau nanya") !== -1) {
-    output.innerHTML = "nanya aja, gua jawab kok";
+  }else if (input.toLowerCase().indexOf("apa kabar") !== -1) {
+    output.innerHTML = "kabar baik, kamu?";
+  } else if (input.toLowerCase().indexOf("alhamdulillah baik") !== -1) {
+    output.innerHTML = "alhamdulillah, seneng denger kabar baik dari kamu";
+  } else if (input.tolowerCare().indexOf("kamu siapa") !== -1) {
+    output.innerHTML = "aku adalah chatbot yang siap membantu kamu";
+  } else if (inputHTML.tolowerCare().indexOf("oh, gitu") !== -1) {
+    output.innerHTML = "iya, gitu deh";
+  } else if (inputHTML.tolowerCare().indexOf("kamu dah makan belum?") !== -1) {
+    output.innerHTML = "aku adalah ai, aku tidak makan dan minum ataupun tidur";
+  } else if (inputHTML.tolowerCare().indexOf("kamu suka aku nggk?") !== -1) {
+    output.innerHTML = "iyaaa gitu deh";
+  } else if (inputHTML.tolowerCare().indexOf("ciaaah") !== -1) {
+    output.innerHTML = "hhhh";
+  } else if (input.toLowerCase().indexOf("mau tanya") !== -1) {
+    output.innerHTML = "tanya ajah, aku jawab kok";
   } else if (input.toLowerCase().indexOf("jadi gini") !== -1) {
     output.innerHTML = "ya udah, gua dengerin";
   } else if (input.toLowerCase().indexOf("bantu gua dong") !== -1) {
@@ -24,3 +38,4 @@ function getResponse() {
   // Biar input-nya kosong lagi setelah kirim
   inputField.value = "";
 }
+
